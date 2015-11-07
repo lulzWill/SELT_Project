03a90f4882a606532a9f50bee49c3d7c8b6eb623
ryantatum <ryant9211@gmail.com>
2015-11-07 03:35:24 +0000

    merging

--- features/LoginUser.feature
+++ features/LoginUser.feature
@@ -1,18 +1,18 @@
 Feature: Allow users to login_path
   
-  Scenario: Login as a Student
-    When I have entered a User-Id "student" and password "student"
-    Then I should be logged in as a "Student" with User-Id "student"
+Scenario: Login as a Student
+  When I have entered UserId "student" and password "student"
+  Then I should be logged in as "Student"
     
-  Scenario: Login as an Teacher
-    When I have entered the User-Id "teacher" and password "teacher"
-    Then I should be logged in as the "Teacher" with User-Id "teacher"
+Scenario: Login as a Teacher
+  When I have entered UserId "teacher" and password "teacher"
+  Then I should be logged in as "Teacher"
     
-  Scenario: Login as an Admin
-    When I have entered User-Id "admin" and password "admin1" 
-    Then I should be logged in as "Admin" with User-Id "admin"
+Scenario: Login as a Admin
+  When I have entered UserId "admin" and password "admin1" 
+  Then I should be logged in as "Admin"
     
-  Scenario: Invalid Login
-    When I have entered this User-Id "doesnot" and password ""
-    Then I should be redirected to login path
+Scenario: Invalid Login
+  When I have entered UserId "doesnot" and password ""
+  Then I should be redirected to login path
     
--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -1,45 +1,12 @@
-Given /^I am on the login page$/ do
-    visit login_create_path
-end
-
-When /^I have entered a User-Id "(.*?)" and password "(.*?)"$/ do |id, password|
-    visit login_create_path
+When /^I have entered UserId "(.*?)" and password "(.*?)"$/ do |id, password|
+    visit login_path
     fill_in 'user_id', :with => id
     fill_in 'password', :with => password
     click_button 'Log in'
 end
 
-Then /^I should be logged in as a "(.*?)" with User-Id "(.*?)"&/ do |role, id|
-    expect(page.text).to match(/Profile for #{role} #{id}/)
-end
-
-When /^I have entered the User-Id "(.*?)" and password "(.*?)"$/ do |id, password|
-    visit login_create_path
-    fill_in 'user_id', :with => id
-    fill_in 'password', :with => password
-    click_button 'Log in'
-end
-
-Then /^I should be logged in as the "(.*?)" with User-Id "(.*?)"&/ do |role, id|
-    expect(page.text).to match(/Profile for #{role} #{id}/)
-end
-
-When /^I have entered User-Id "(.*?)" and password "(.*?)"$/ do |id, password|
-    visit login_create_path
-    fill_in 'user_id', :with => id
-    fill_in 'password', :with => password
-    click_button 'Log in'
-end
-
-Then /^I should be logged in as "(.*?)" with User-Id "(.*?)"&/ do |role, id|
-    expect(page.text).to match(/Profile for #{role} #{id}/)
-end
-
-When /^I have entered this User-Id "(.*?)" and password "(.*?)"$/ do |id, password|
-    visit login_create_path
-    fill_in 'user_id', :with => id
-    fill_in 'password', :with => password
-    click_button 'Log in'
+Then /^I should be logged in as "(.*?)"$/ do |role|
+    expect(page.text).to match(/Invalid/)
 end
     
 Then /^I should be redirected to login path$/ do
