9f191eb5ad481d3bc1da2dd35dbc6c2d80ed81b0
lulzwill <william-pruyn@uiowa.edu>
2015-11-20 02:26:16 +0000

    grades can be seen and added to the database. needs some client/server side validation and tests

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -6,19 +6,19 @@ class GradesController < ApplicationController
     end
     
     def create
-        if(Assignment.find(params[:grade][:assignment_id])) 
-            assignment = Assignment.find(params[:grade][:assignment_id])
-            attributes_to_update = {:grades => assignment.grades.merge(params[:grade][:assignment_id] => params[:grade][:points])}
+        if(Assignment.find(params[:assignment_id])) 
+            assignment = Assignment.find(params[:assignment_id])
+            attributes_to_update = {:grades => assignment.grades.merge(params[:grade][:user_id] => params[:grade][:points])}
             if assignment.update_attributes!(attributes_to_update)
-                flash[:message] = "successfully added grade for " + params_hash[:grade][:user_id] + "!"
-                #don't know where to redirect_to 
+                flash[:notice] = "successfully added grade for " + params[:grade][:user_id] + "!"
+                #redirect_to login_path
             else
-                flash[:message] = "successfully added grade for " + params_hash[:grade][:user_id] + "!"
-                #something went horribly wrong
+                flash[:notice] = "successfully added grade for " + params[:grade][:user_id] + "!"
+                #redirect_to login_path
             end
         else
-            flash[:message] = "Assignment does not exist!"
-            #don't know where to redirect_to
+            flash[:notice] = "Assignment does not exist!"
+            #redirect_to login_path
         end
     end
     
